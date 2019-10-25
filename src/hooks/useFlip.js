996bc4239a4986973296e1bb05b9be9eaf8e41c1
jlkiri <rensakamoto7@gmail.com>
2019-10-26 02:21:20 +0900

    Simplify in-flight detection logic

--- src/hooks/useFlip.js
+++ src/hooks/useFlip.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useLayoutEffect } from "react";
+import { useRef, useEffect, useCallback, useLayoutEffect } from "react";
 
 const debounce = function debounce(fn) {
   let timer;
@@ -10,9 +10,22 @@ const debounce = function debounce(fn) {
 
 export default function useFlipAnimation({ root, opts, deps }) {
   const childCoords = useRef({ refs: Object.create(null) });
-  const canonicalPositions = useRef(Object.create(null));
 
   const transition = opts.transition || 500;
+
+  const onTransitionEnd = useCallback(function onTransitionEnd(e) {
+    const targetKey = e.target.dataset.id;
+    childCoords.current.refs[targetKey] = e.target.getBoundingClientRect();
+    e.target.inFlight = false;
+    e.target.removeEventListener("transitioncancel", onTransitionCancel);
+    e.target.removeEventListener("transitionend", onTransitionEnd);
+  }, []);
+
+  const onTransitionCancel = useCallback(function onTransitionCancel(e) {
+    e.target.inFlight = false;
+    e.target.removeEventListener("transitionend", onTransitionEnd);
+    e.target.removeEventListener("transitioncancel", onTransitionCancel);
+  }, []);
 
   useEffect(() => {
     const onResize = debounce(() => {
@@ -31,45 +44,13 @@ export default function useFlipAnimation({ root, opts, deps }) {
   }, [root]);
 
   useLayoutEffect(() => {
-    const children = root.current.children;
-
-    if (children.length < 1) return;
-
-    // Save "canonical" coordinates on first render
-    // These will be used to prevent transforms of in-flight elements
-    for (let child of children) {
-      canonicalPositions.current[
-        child.dataset.id
-      ] = child.getBoundingClientRect();
-    }
-  }, [root]);
-
-  useLayoutEffect(() => {
-    const canonicalTop = Object.values(canonicalPositions.current).map(
-      pos => pos.top
-    );
-    const canonicalLeft = Object.values(canonicalPositions.current).map(
-      pos => pos.left
-    );
-
-    const minTop = Math.min(...canonicalTop);
-    const maxTop = Math.max(...canonicalTop);
-    const minLeft = Math.min(...canonicalLeft);
-    const maxLeft = Math.max(...canonicalLeft);
-
     const play = function play(elem) {
       elem.style.transform = ``;
       elem.style.transition = `transform ${transition}ms`;
+      elem.inFlight = true;
 
       // Update saved DOM position on transition end to prevent
       // "in-flight" positions saved as previous
-      const onTransitionEnd = function onTransitionEnd() {
-        childCoords.current.refs[
-          elem.dataset.id
-        ] = elem.getBoundingClientRect();
-        elem.removeEventListener("transitionend", onTransitionEnd);
-      };
-
       elem.addEventListener("transitionend", onTransitionEnd);
     };
 
@@ -77,6 +58,8 @@ export default function useFlipAnimation({ root, opts, deps }) {
       return function _invert({ dx, dy }) {
         elem.style.transform = `translate(${dx}px, ${dy}px)`;
         elem.style.transition = `transform 0s`;
+
+        elem.addEventListener("transitioncancel", onTransitionCancel);
       };
     };
 
@@ -104,31 +87,9 @@ export default function useFlipAnimation({ root, opts, deps }) {
           const deltaX = prevX - nextX;
           const deltaY = prevY - nextY;
 
-          const noOverflowY =
-            nextY >= minTop &&
-            nextY <= maxTop &&
-            prevY >= minTop &&
-            prevY <= maxTop;
-          const noOverflowX =
-            nextX >= minLeft &&
-            nextX <= maxLeft &&
-            prevX >= minLeft &&
-            prevX <= maxLeft;
+          invert(child)({ dx: deltaX, dy: deltaY });
 
-          // Best effort to prevent transform of the children currently in animation
-          // Otherwise, some children may move out of the viewport
-          if (
-            noOverflowY &&
-            noOverflowX &&
-            canonicalTop.includes(nextY) &&
-            canonicalLeft.includes(nextX) &&
-            canonicalTop.includes(prevY) &&
-            canonicalLeft.includes(prevX)
-          ) {
-            invert(child)({ dx: deltaX, dy: deltaY });
-
-            requestAnimationFrame(() => play(child));
-          }
+          requestAnimationFrame(() => play(child));
         }
       }
     });
@@ -136,7 +97,9 @@ export default function useFlipAnimation({ root, opts, deps }) {
     // Save new DOM positions
     for (let child of children) {
       const key = child.dataset.id;
-      childCoords.current.refs[key] = child.getBoundingClientRect();
+      if (!child.inFlight) {
+        childCoords.current.refs[key] = child.getBoundingClientRect();
+      }
     }
   }, [deps, transition, root]);
 }
