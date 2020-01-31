2114295136065fd173d96c85b27c87c61d7296c4
Kirill Vasiltsov <v_kirill@yumemi.co.jp>
2020-01-31 19:05:35 +0900

    Move assets

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { TestApp } from './TestApp'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<TestApp />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
